4fc79d1c2f2f7d8d8bd4d79d59306cc15664a803
Ritik Shah <ritik10.shah@gmail.com>
2019-05-16 15:49:30 -0700

    rx: major rework.
    new:
      uses lazy generators to avoid taking space in memory

--- mapper.py
+++ mapper.py
@@ -1,75 +1,138 @@
 from base64 import b16encode
+from pathlib import Path
+from itertools import chain
 import pygraphviz as pgv
 import random
+import json
 import time
-import os
+import re
+
+fcount = 0
 
 
 def gen_pastel_color():
     triplet = (
-            round((random.randrange(0, 255) + 255) / 2),
-            round((random.randrange(0, 255) + 255) / 2),
-            round((random.randrange(0, 255) + 255) / 2)
+        round((random.randrange(0, 255) + 255) / 2),
+        round((random.randrange(0, 255) + 255) / 2),
+        round((random.randrange(0, 255) + 255) / 2)
     )
-    return str(b'#'+b16encode(bytes(triplet)))[1:].replace("'", '').lower().strip()
+    return str(b'#'+b16encode(bytes(triplet)))[1:].replace("'", '').lower().strip()  # noqa
 
 
-def get_function(fname):
-    name = fname[fname.find('data/') + 5:]
-    slash = name.find('/')
-    name = name[:slash] + ':' + name[slash + 11:]  # / -> : + functions/ -> ''
-    calls = []
-    with open(fname) as file:
-        lines = file.read().split('\n')
+def trace(source, lead='line'):
+    for item in source:
+        print(lead + ': ' + str(item))
+        yield item
 
+
+def gen_open(paths):
+    global fcount
+    for path in paths:
+        fcount += 1
+        yield path.open()
+
+def convert(fname, num=4):
+    parts = Path(fname).parts  # 0:1 - datapack/data, 3 - functions
+    return parts[2] + ':' + '/'.join(re.sub(r'(\.mcfunction)|(\.json)', '', part) for part in parts[num:])
+
+
+
+def gen_lines(files):
+    for file in files:
+        lines = file.readlines()
         for line in lines:
-            line = line.strip()
-            if line != '' and line[0] != '#' and 'function' in line:
-                index = line.find('function')
-                connection = line[:index].replace(' run', '')
-                connection = connection.replace('execute ', '').strip()
-                call_file = line[index + len('function'):].strip()  # noqa
-
-                calls.append((call_file, connection))
-
-    return name, calls
+            yield convert(file.name), line
 
 
-if __name__ == '__main__':
+def gen_grep(pat, tups):
+    for name, line in tups:
+        if pat.search(line):
+            yield name, line.replace(' run', '').replace('execute ', '').strip()  # noqa
+
+
+def find_namespace(s):
+    pass
+
+
+
+def gen_do(pat, tup):
+    for name, line in tup:
+        match = pat.search(line)
+        if match.group(1).strip() == 'schedule function':
+            time = match.group(4).strip()
+        else:
+            time = ''
+
+        func = match.group(3)
+        label = line[:match.start()].strip() + ' ' + time
+        yield name, (func, label)
+
+
+def gen_json(paths):
+    for path in paths:
+        namespaced = '#' + convert(path, 5)  # path.__repl__() -> str
+        jfile = json.load(path.open())
+        yield ((namespaced, (val, '')) for val in jfile['values'])
+
+
+def main():
+    fecount = 0
+    pat = re.compile(r'^((?!^#.+).)*$')
+    patf = re.compile(r'((schedule )?function(?![^{]*})) (#?[a-z0-9.-_+:]+)( \d+.)?')  # noqa
+
     dir_name = str(input('Datapack name: '))
-    start_time = int(time.time())
-    funcs = []
-    print('Starting Dir Walk')
-    for root, _, files in os.walk(dir_name):
-        for file in files:
-            fname = root + '/' + file
-            if '.mcfunction' in fname:
-                funcs.append(get_function(fname))
-    print('func -> dict')
-    funcs = dict(funcs)
-    print('Generating graph attributes')
+    start_time = time.time()
+
+    funcfames = Path(f'./{dir_name}/data').rglob('*.mcfunction')
+    funcfiles = gen_open(funcfames)
+    functuple = gen_lines(funcfiles)
+    funclines = gen_grep(patf, functuple)
+    funcfuncs = gen_grep(pat, funclines)
+    functions = gen_do(patf, funcfuncs)
+
+    jsonfames = Path(f'./{dir_name}/data').rglob('*/tags/functions/*.json')
+    functagss = gen_json(jsonfames)
+    finaltags = chain.from_iterable(functagss)
+
+    gen_funcs = chain(functions, finaltags)  # Final stop
+
     G = pgv.AGraph(splines=True,
                    overlap=False,
-#                  overlap='scale',
+#                  overlap='scale',  # noqa
                    strict=False,
                    directed=True,
                    bgcolor='#262626')
+
     G.node_attr.update(color='white', fontcolor='#bfbfbf')
-    # G.edge_attr.update(color='white', fontcolor='#bfbfbf')
-    print(f'Building Graph with {len(funcs)} functions')
-    for name, calls in funcs.items():
-        name = name.replace('.mcfunction', '').strip()
-        G.add_node(name)
-        for call in calls:
-            color = gen_pastel_color()
-            G.add_edge(name, call[0], color=color, fontcolor=color)
-    # G = G.unflatten('-f -l 1')
-    # timeit()
+
+    print('Building graph')
+    # test = set()
+    for func in gen_funcs:
+        fecount += 1
+        name, call = func
+        G.add_node(name.strip())
+        # test.add(name.strip() + '\n')
+        called = call[0]
+        if called != '':
+            G.add_edge(name, called.strip(), color=gen_pastel_color())
+
+    '''
+    with open('text.txt', 'w') as file:
+        out = ''
+        for item in test:
+            out += item
+        file.write(out)
+    '''
+
+    print(f'Built with: {fcount} functions and {fecount} connections ({G.order()} nodes)')  # noqa
     print('Laying graph out')
     G.layout()
     print('Writing to .dot file')
-    G.write(f"{dir_name}.dot")
+    G.write(f'dots/{dir_name}.dot')
     print('Drawing to jpeg')
     G.draw(f'pics/{dir_name}.jpeg', format='jpeg', prog='sfdp')
-    total = abs(start_time - int(time.time()))
-    print(f'Done in {total}s!')
+    print(f'Done in {round(abs(start_time - time.time()), 3)}s!')
+
+
+if __name__ == '__main__':
+    main()
