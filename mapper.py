30b1d2cc145000810c95113188b4ad983f118f6f
Ritik Shah <ritik10.shah@gmail.com>
2019-05-17 10:03:59 -0700

    rx: commented and fixed a bug
    bug:
      returned single tuple rather than tuple of tuple

--- mapper.py
+++ mapper.py
@@ -12,17 +12,25 @@ import re
 
 fcount = 0
 
+"""
+TODO: use logger?
+TODO: global variables, but it's probably ok
+"""
+
 
 def gen_pastel_color():
+    """ generates pastel colors through a formula (adding 255 then halving """
     triplet = (
         round((random.randrange(0, 255) + 255) / 2),
         round((random.randrange(0, 255) + 255) / 2),
         round((random.randrange(0, 255) + 255) / 2)
     )
+    # transform triple to '#ffffff' form
     return str(b'#'+b16encode(bytes(triplet)))[1:].replace("'", '').lower().strip()  # noqa
 
 
 def trace(source, lead=None):
+    """ debug generator that can wrap any generator to print items """
     if lead is None:
         lead = source.__name__
     for item in source:
@@ -31,11 +39,13 @@ def trace(source, lead=None):
 
 
 def convert(fname, num=4):
+    """ converts file path to mc namespaced path """
     parts = Path(fname).parts  # 0:1 - datapack/data, 3 - functions
     return parts[2] + ':' + '/'.join(re.sub(r'(\.mcfunction)|(\.json)', '', part) for part in parts[num:])  # noqa
 
 
 def gen_open(paths):
+    """ opens path and returns 'em """
     global fcount
     for path in paths:
         fcount += 1
@@ -43,19 +53,27 @@ def gen_open(paths):
 
 
 def gen_lines(files):
+    """ reads file into lines and yields a tuple: (mc path, line) """
     for file in files:
         lines = file.readlines()
+        name = file.name
+        file.close()
         for line in lines:
-            yield convert(file.name), line
+            yield convert(name), line
 
 
 def gen_grep(pat, tups):
+    """ filter input through pattern searching """
+    # TODO: move .replace's elsewhere
     for name, line in tups:
         if pat.search(line):
             yield name, line.replace(' run', '').replace('execute ', '').strip()  # noqa
 
 
 def gen_do(pat, tup):
+    """
+    takes in the func pattern and yields each mc name with it's call and label
+    """
     for name, line in tup:
         match = pat.search(line)
         if match.group(1).strip() == 'schedule function':
@@ -65,29 +83,33 @@ def gen_do(pat, tup):
 
         func = match.group(3)
         label = line[:match.start()].strip() + ' ' + time
-        yield name, (func, label)
+        yield name, func, label
 
 
 def gen_tag(paths):
+    """ takes in paths for func tags and yields the name and what it calls """
     for path in paths:
         namespaced = '#' + convert(path, 5)  # path.__repl__() -> str
         jfile = json.load(path.open())
         for val in jfile['values']:
-            yield (namespaced, (val, ''))
+            yield namespaced, val, ''
 
 
 def gen_adv(paths):
+    """ takes in paths for advs and yields the name and the reward """
     for path in paths:
         jfile = json.load(path.open())
         if 'rewards' in jfile:  # rewards has the function output
-            yield (str(path), (jfile['rewards']['function'], ''))
+            yield str(path), jfile['rewards']['function'], ''
 
 
 def get_paths(dir_name, glob):
+    """ returns a generator of the recursive paths on input glob """
     return Path(f'./{dir_name}/data').rglob(glob)
 
 
 def get_functions(dir_name):
+    """ packaged generators that handle functions """
     pat = re.compile(r'^((?!^#.+).)*$')
     patf = re.compile(r'((schedule )?function(?![^{]*})) (#?[a-z0-9.-_+:]+)( \d+.)?')  # noqa
     funcnames = get_paths(dir_name, '*/functions/**/*.mcfunction')
@@ -99,68 +121,76 @@ def get_functions(dir_name):
 
 
 def get_tags(dir_name):
+    """ packaged generators that handle function tags """
     jsonnames = get_paths(dir_name, '*/tags/functions/*.json')
     return gen_tag(jsonnames)
 
 
 def get_adv(dir_name):
+    """ packaged generators that handle advancements """
     advjnames = get_paths(dir_name, '*/advancements/**/*.json')
     return gen_adv(advjnames)
 
 
 def stream_nodes(datapack):
+    """ combines all three packages in one generator stream """
     functions = get_functions(datapack)
     functags = get_tags(datapack)
     advnames = get_adv(datapack)
     return chain(functions, functags, advnames)
 
 
-def build_graph(G, gen, label, datapack):
+def build_graph(G, gen, lab, datapack):
+    """ builds graphviz graph through a generator """
     fecount = 0
 
     for func in gen:
         fecount += 1
-        name, call = func
+        name, call, label = func
         if name.startswith('#') or '.json' in name:  # if tag or adv
             color = gen_pastel_color()
             G.add_node(name.strip(), color=color, fontcolor=color)
         else:
             G.add_node(name.strip())
-        called = call[0]
-        if called != '':
+
+        if call != '':
             color = gen_pastel_color()
-            if label:
-                G.add_edge(name, called.strip(),
-                           color=color, fontcolor=color, label=call[1]
-                )
+            if lab:
+                G.add_edge(name, call.strip(),
+                           color=color, fontcolor=color, label=label)
             else:
-                G.add_edge(name, called.strip(),
-                           color=color
-                )
+                G.add_edge(name, call.strip(),
+                           color=color)
 
     print(f'  {datapack} built with: {fcount} functions and {fecount} connections ({G.order()} nodes)!')  # noqa
 
 
 def output_graph(dir_name, G, outfile):
+    """ displays the graph to jpeg (and .dot) """
     if outfile is None:
-        outfile = dir_name
+        outfile = dir_name  # dir_name can be our outfile
     print(f'Constructing the {outfile} graph. Warning: This might take a while')  # noqa
     print('  Laying graph out')
-    G.layout()
+    G.layout()  # lays out the graph. not neccissary, but helps with final product # noqa
     print(f'  Writing to {outfile}.dot')
-    G.write(f'{outfile}.dot')
+    G.write(f'{outfile}.dot')  # this file can be displayed again through any graphviz program # noqa
     print(f'  Drawing to {outfile}.jpeg')
     G.draw(f'{outfile}.jpeg', format='jpeg', prog='sfdp')
+
 
 def main(datapacks, mode='one', label=False, outfile=None):
     global fcount
     start = time.time()
-    
+
+    # labeling doesn't work properly on smart overlap (=False)
+    # on scale, the output picture get's pretty big, but looks neater
     if label:
         overlap = 'scale'
     else:
         overlap = False
 
+    # mode: one, loop each datapack on building then output
+    # mode: multiple, loop each datapack and output each one
     if mode == 'one':
         G = pgv.AGraph(splines=True,
                        overlap=overlap,
@@ -178,22 +208,25 @@ def main(datapacks, mode='one', label=False, outfile=None):
         output_graph(datapacks[0], G, outfile)
     elif mode == 'multiple':
         for datapack in datapacks:
+            # strict=False and directed=True allows self-loops
             G = pgv.AGraph(splines=True,
                            overlap=overlap,
                            strict=False,
                            directed=True,
                            bgcolor='#262626')
-
             G.node_attr.update(color='white', fontcolor='#bfbfbf')
+
             print('Reading in the datapack and building the graph')
             nodes = stream_nodes(datapack)
-            build_graph(G, nodes, label)
-            output_graph(datapack, G, outfile)
+            build_graph(G, nodes, label, None)  # None, otherwise each file named same # noqa
+            output_graph(datapack, G)
             fcount = 0
     else:
         print('Invalid Mode')
+        print('Please notify the repo maintainers if you see this')
+        print('Stopping..')
+        sys.exit()
 
-    
     print(f'Done in {round(abs(start - time.time()), 3)}s!')
 
 
